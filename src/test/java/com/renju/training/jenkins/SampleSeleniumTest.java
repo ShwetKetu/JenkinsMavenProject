131001559beb78f5edaa532144d5e519781ef597
ShwetKetu <ketu1009@gmail.com>
2023-01-24 12:03:33 +0530

    Update SampleSeleniumTest.java

--- src/test/java/com/renju/training/jenkins/SampleSeleniumTest.java
+++ src/test/java/com/renju/training/jenkins/SampleSeleniumTest.java
@@ -28,7 +28,7 @@ public class SampleSeleniumTest {
 		driver.get("http://www.google.com");
 		System.out.println("Clicking Gmail Link");
 		driver.findElement(By.xpath("//*[@id=\"gbw\"]/div/div/div[1]/div[1]/a")).click();
-		System.out.println("Clicking Sign In link");
+		System.out.println("Clicking Sign In here link");
 		driver.findElement(By.xpath("/html/body/nav/div/a[2]")).click();
 		System.out.println("Entering username");
 		driver.findElement(By.xpath("//*[@id=\"identifierId\"]")).sendKeys("renju.jenkins.training");
